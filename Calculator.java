2d597d619d5cae4601c53abb4b25b4e2753ee73c
Ashish <ashishs1460@gmail.com>
2022-12-27 22:58:39 +0530

    JFrame imported and main function created

--- Calculator.java
+++ Calculator.java
@@ -1,4 +1,15 @@
+import javax.swing.JFrame;
 
 public class Calculator {
-
+public Calculator() {
+	JFrame jf =new JFrame("Calculator");
+	jf.setLayout(null);
+	jf.setSize(600,600);
+	
+	jf.setVisible(true);
+	jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+}
+public static void main (String ar[]) {
+	Calculator c= new Calculator();
+}
 }
